6b6725f8876498a4c5953877e98599a2b4ecfd63
Vladimir Bondar <vladi.bondar@gmail.com>
2021-06-07 00:04:17 +0300

    Fix header

--- src/js/site-nav.js
+++ src/js/site-nav.js
@@ -1,18 +1,21 @@
 (() => {
-  const menuBtnRef = document.querySelector("[data-nav-btn]");
+  const navBtnRef = document.querySelector("[data-nav-btn]");
   const buyBtn = document.querySelector("[data-buy-btn]");
+  const buyBtnMob = document.querySelector("[data-buy-btn-mob]");
+  
   const page = document.querySelector("[data-nav-page]");
-  const mobileMenuRef = document.querySelector("[data-nav]");
+  const siteNavRef = document.querySelector("[data-nav]");
 
-  menuBtnRef.addEventListener("click", () => {
-    const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+  navBtnRef.addEventListener("click", () => {
+    const expanded = navBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-    menuBtnRef.classList.toggle("site-nav-btn--nav-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
+    navBtnRef.classList.toggle("site-nav-btn--nav-open");
+    navBtnRef.setAttribute("aria-expanded", !expanded);
 
     buyBtn.classList.toggle("page-header__btn--nav-open");
+    buyBtnMob.classList.toggle("page-header__btn-mob--nav-open");
 
     page.classList.toggle("page--nav-open");
-    mobileMenuRef.classList.toggle("backdrop--is-open");
+    siteNavRef.classList.toggle("backdrop--is-open");
   });
 })();
