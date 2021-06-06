33a0bd7ac180170a545ea43f9b569d76259003d5
Vladimir Bondar <vladi.bondar@gmail.com>
2021-06-06 10:33:00 +0300

    Fix site nav position

--- src/js/site-nav.js
+++ src/js/site-nav.js
@@ -1,18 +1,18 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-nav-btn]");
   const buyBtn = document.querySelector("[data-buy-btn]");
-  const mobileMenuRef = document.querySelector("[data-nav]");
   const page = document.querySelector("[data-nav-page]");
+  const mobileMenuRef = document.querySelector("[data-nav]");
 
   menuBtnRef.addEventListener("click", () => {
     const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-    menuBtnRef.classList.toggle("site-nav-btn--is-open");
+    menuBtnRef.classList.toggle("site-nav-btn--nav-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
     buyBtn.classList.toggle("page-header__btn--nav-open");
 
-    mobileMenuRef.classList.toggle("backdrop--is-open");
     page.classList.toggle("page--nav-open");
+    mobileMenuRef.classList.toggle("backdrop--is-open");
   });
 })();
