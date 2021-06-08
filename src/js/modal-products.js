66061c62c88ecc240d00d3ce3fa98d940340b4de
Anna-Sergeevna <4042808@gmail.com>
2021-06-08 23:07:08 +0300

    Добавляет модалку на 3 карточки

--- src/js/modal-products.js
+++ src/js/modal-products.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[data-modal-open-ice-cream]'),
+    closeModalBtn: document.querySelector('[data-modal-close-ice-cream]'),
+    modal: document.querySelector('[data-modal-ice-cream]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -10,6 +10,38 @@
 
   function toggleModal() {
     document.body.classList.toggle('modal-open');
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('product__description--is-hidden');
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-ice-coffee]'),
+    closeModalBtn: document.querySelector('[data-modal-close-ice-coffee]'),
+    modal: document.querySelector('[data-modal-ice-coffee]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle('modal-open');
+    refs.modal.classList.toggle('product__description--is-hidden');
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-milkshakes]'),
+    closeModalBtn: document.querySelector('[data-modal-close-milkshakes]'),
+    modal: document.querySelector('[data-modal-milkshakes]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle('modal-open');
+    refs.modal.classList.toggle('product__description--is-hidden');
   }
 })();
