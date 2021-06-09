9d969b033edd09a5d1c4d01a6ec6ba5e1693a5e3
Eduard-Konovka <ed098ua@gmail.com>
2021-06-09 16:21:58 +0300

    Добавляет Slick Slider

--- src/js/slick-script.js
+++ src/js/slick-script.js
@@ -1,5 +1,7 @@
-$(document).ready(function(){
-  $('.your-class').slick({
-    setting-name: setting-value
-  });
+$('.reviews__slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
 });
