09766c2b64a4f3976e6f71b1a81373f3957a2a62
Vladimir Bondar <vladi.bondar@gmail.com>
2021-06-09 03:56:33 +0300

    Connect modal-locations

--- src/js/modal-locations.js
+++ src/js/modal-locations.js
@@ -1,14 +1,15 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[data-modal-open-locations]'),
+    closeModalBtn: document.querySelector('[data-modal-close-locations]'),
+    modal: document.querySelector('[data-modal-locations]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('page__modal-open');
+    refs.modal.classList.toggle('backdrop-locations--is-hidden');
   }
 })();
