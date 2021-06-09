fdcd0ff7f084a610ad6e1a22e538c7b21a272c96
Vladimir Bondar <vladi.bondar@gmail.com>
2021-06-09 15:25:23 +0300

    Add site-nav sensibility on link click

--- src/js/site-nav.js
+++ src/js/site-nav.js
@@ -1,5 +1,12 @@
 (() => {
   const navBtnRef = document.querySelector("[data-nav-btn]");
+
+  const navLinkHomeRef = document.querySelector("[data-nav-link-home]");
+  const navLinkAdvantagesRef = document.querySelector("[data-nav-link-advantages]");
+  const navLinkAboutRef = document.querySelector("[data-nav-link-about]");
+  const navLinkProductsRef = document.querySelector("[data-nav-link-products]");
+  const navLinkLocationsRef = document.querySelector("[data-nav-link-locations]");
+  
   const buyBtn = document.querySelector("[data-buy-btn]");
   const buyBtnMob = document.querySelector("[data-buy-btn-mob]");
   
@@ -7,6 +14,71 @@
   const siteNavRef = document.querySelector("[data-nav]");
 
   navBtnRef.addEventListener("click", () => {
+    const expanded = navBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    navBtnRef.classList.toggle("is-active");
+    navBtnRef.setAttribute("aria-expanded", !expanded);
+
+    buyBtn.classList.toggle("btn-buy--nav-open");
+    buyBtnMob.classList.toggle("btn-buy-mob--nav-open");
+
+    page.classList.toggle("page--nav-open");
+    siteNavRef.classList.toggle("site-nav-backdrop--is-open");
+  });
+
+  navLinkHomeRef.addEventListener("click", () => {
+    const expanded = navBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    navBtnRef.classList.toggle("is-active");
+    navBtnRef.setAttribute("aria-expanded", !expanded);
+
+    buyBtn.classList.toggle("btn-buy--nav-open");
+    buyBtnMob.classList.toggle("btn-buy-mob--nav-open");
+
+    page.classList.toggle("page--nav-open");
+    siteNavRef.classList.toggle("site-nav-backdrop--is-open");
+  });
+
+  navLinkAdvantagesRef.addEventListener("click", () => {
+    const expanded = navBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    navBtnRef.classList.toggle("is-active");
+    navBtnRef.setAttribute("aria-expanded", !expanded);
+
+    buyBtn.classList.toggle("btn-buy--nav-open");
+    buyBtnMob.classList.toggle("btn-buy-mob--nav-open");
+
+    page.classList.toggle("page--nav-open");
+    siteNavRef.classList.toggle("site-nav-backdrop--is-open");
+  });
+
+  navLinkAboutRef.addEventListener("click", () => {
+    const expanded = navBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    navBtnRef.classList.toggle("is-active");
+    navBtnRef.setAttribute("aria-expanded", !expanded);
+
+    buyBtn.classList.toggle("btn-buy--nav-open");
+    buyBtnMob.classList.toggle("btn-buy-mob--nav-open");
+
+    page.classList.toggle("page--nav-open");
+    siteNavRef.classList.toggle("site-nav-backdrop--is-open");
+  });
+
+  navLinkProductsRef.addEventListener("click", () => {
+    const expanded = navBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    navBtnRef.classList.toggle("is-active");
+    navBtnRef.setAttribute("aria-expanded", !expanded);
+
+    buyBtn.classList.toggle("btn-buy--nav-open");
+    buyBtnMob.classList.toggle("btn-buy-mob--nav-open");
+
+    page.classList.toggle("page--nav-open");
+    siteNavRef.classList.toggle("site-nav-backdrop--is-open");
+  });
+
+  navLinkLocationsRef.addEventListener("click", () => {
     const expanded = navBtnRef.getAttribute("aria-expanded") === "true" || false;
 
     navBtnRef.classList.toggle("is-active");
