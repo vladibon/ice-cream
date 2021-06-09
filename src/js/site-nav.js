03b3e376908689cde65eb9e83fa3aa9e6b431c0e
Vladimir Bondar <vladi.bondar@gmail.com>
2021-06-09 14:37:39 +0300

    Add button top

--- src/js/site-nav.js
+++ src/js/site-nav.js
@@ -12,10 +12,33 @@
     navBtnRef.classList.toggle("is-active");
     navBtnRef.setAttribute("aria-expanded", !expanded);
 
-    buyBtn.classList.toggle("page-header__btn--nav-open");
-    buyBtnMob.classList.toggle("page-header__btn-mob--nav-open");
+    buyBtn.classList.toggle("btn-buy--nav-open");
+    buyBtnMob.classList.toggle("btn-buy-mob--nav-open");
 
     page.classList.toggle("page--nav-open");
     siteNavRef.classList.toggle("site-nav-backdrop--is-open");
   });
+})();
+
+(() => {
+  var topButton = document.getElementById('myBtn');
+
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  topButton.addEventListener('click', topFunction);
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      topButton.style.display = 'block';
+    } else {
+      topButton.style.display = 'none';
+    }
+  }
+
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
 })();
